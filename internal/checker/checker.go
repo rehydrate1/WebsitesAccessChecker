76abf2a3a49713a146a156c3a657441fff7399c8
Ushakov Artem <artom444fta@yandex.ru>
2025-04-26 21:57:31 +0300

    develop: added headers to reqest

--- internal/checker/checker.go
+++ internal/checker/checker.go
@@ -1,6 +1,7 @@
 package checker
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -16,16 +17,33 @@ type CheckResult struct {
 func CheckSite(url string) CheckResult {
 	result := CheckResult{URL: url}
 	client := http.Client{Timeout: 10 * time.Second}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		result.IsAvailable = false
+		result.Error = fmt.Sprintf("Ошибка при создании запроса: %v", err)
+		return result
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 YaBrowser/25.2.0.0 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Accept-Language", "ru,en;q=0.9")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br, zstd")
+	req.Header.Set("Sec-Ch-Ua", `"Not A(Brand";v="8", "Chromium";v="132", "YaBrowser";v="25.2", "Yowser";v="2.5"`)
+	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+	req.Header.Set("Sec-Ch-Ua-Platform", "Windows")
+	
 	startTime := time.Now()
 
-	resp, err := client.Get(url)
-	result.ResponseTimeMs = time.Since(startTime).Milliseconds()
+	resp, err := client.Do(req)
 	if err != nil {
 		result.IsAvailable = false
 		result.Error = err.Error()
 		return result
 	}
 	defer resp.Body.Close()
+
+	result.ResponseTimeMs = time.Since(startTime).Milliseconds()
 
 	result.StatusCode = resp.StatusCode
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
